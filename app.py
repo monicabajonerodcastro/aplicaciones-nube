e4481e6f6a4efd56f275fc01f7f55f90900d7a6f
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2023-04-01 14:50:14 -0500

    Commit inicial
    Commit inicial

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/api/auth/signup', methods = ['POST'])
+def signup():
+    return "POST - sign up"
+
+@app.route('/api/auth/login', methods = ['POST'])
+def login():
+    return "POST - login"
+
+@app.route('/api/tasks', methods = ['GET', 'POST'])
+def tasks():
+    if request.method == 'POST':
+        return "POST - tasks"
+    else:
+        return "GET - tasks"
+    
+@app.route('/api/tasks/<id_task>', methods = ['GET', 'DELETE'])
+def task(id_task):
+    if request.method == 'DELETE':
+        return "DELETE - task by id {}".format(id_task)
+    else:
+        return "GET - task by id {}".format(id_task)
+    
+@app.route('/api/files/<filename>')
+def filename(filename):
+    return "GET - filename"
