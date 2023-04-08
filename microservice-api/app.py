7ed255c2464aee8f4f5daa1eb1c67bf4eb26df91
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2023-04-08 18:00:44 -0500

    Se envía la tarea a la cola y se guarda en la base de datos

--- microservice-api/app.py
+++ microservice-api/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request
 from Models import db
+from service import create_task, save_task_request
+import json
 
 # Configuration
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///application.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 
 db.init_app(app)
 
-# Endpoints
+# Public Endpoints
 @app.route('/api/auth/signup', methods = ['POST'])
 def signup():
     return "POST - sign up"
@@ -20,7 +24,7 @@ def login():
 @app.route('/api/tasks', methods = ['GET', 'POST'])
 def tasks():
     if request.method == 'POST':
-        return "POST - tasks"
+        return create_task(request)
     else:
         return "GET - tasks"
     
@@ -34,3 +38,16 @@ def task(id_task):
 @app.route('/api/files/<filename>')
 def filename(filename):
     return "GET - filename: {}".format(filename)
+
+# Private endpoints
+@app.route('/save-task', methods = ['POST'])
+def save_task():
+    save_task_request(request)
+    message = {
+        "status": 0,
+        "message": "Task creado en la base de datos" 
+    }
+    return json.dumps(message), 200
+
+with app.app_context():
+    db.create_all()
