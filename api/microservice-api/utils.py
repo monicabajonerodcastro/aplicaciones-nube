c93c6f5085c9e2334233580821a14d0c0a0372f4
Juan Sebastian Becerra <j.becerrab@uniandes.edu.co>
2023-05-03 21:34:17 -0500

    feat: agregar metodos utilitarios para manejar envio y descarga de archivos al bucket de gcp

--- api/microservice-api/utils.py
+++ api/microservice-api/utils.py
@@ -8,6 +8,14 @@ else: sys.path.append(os.path.join("/",'constants'))
 import pika, json, zipfile
 from constants import HOST_RABBIT_MQ, UPLOAD_PROCESSED_FOLDER
 
+import os
+from google.cloud import storage
+
+#TODO -> el valor es la ubicacion del archivo llave para acceder al bucket, debe cambiarse por la ruta de la
+#TODO -> llave y agregar ese archivo llave tipo JSON al docker
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'archivos-384418-746a4b2b24fa.json'
+storage_client = storage.Client()
+
 def publish_message(queue, message):
     connection = pika.BlockingConnection(pika.ConnectionParameters(HOST_RABBIT_MQ))
     channel = connection.channel()
@@ -31,3 +39,31 @@ def compress_local_file(path, new_format):
     my_zip.write(path, compress_type=zipfile.ZIP_DEFLATED)
     my_zip.close()
     return UPLOAD_PROCESSED_FOLDER.format(file_name_no_extension, new_format)
+
+def test_connect_ucket(bucket_name):
+    my_bucket = storage_client.get_bucket(bucket_name)
+    print(vars(my_bucket))
+
+def send_to_bucket(blob_name, file_path, bucket_name):
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(file_path)
+    return blob
+
+def get_file_from_bucket(blob_name, file_path, bucket_name):
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    with open(file_path, 'wb') as f:
+        storage_client.download_blob_to_file(blob, f)
+
+def get_file_path_GCP(url, file_path):
+    with open(file_path, 'wb') as f:
+        storage_client.download_blob_to_file(url, f)
+
+def my_compress_file_GCP(bucket_name, file_name):
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.get_blob(file_name)
+    object_bytes = blob.download_as_string()
+    myZip = zipfile.ZipFile('myZip.zip', 'w')
+    myZip.writestr(file_name, object_bytes, compress_type=zipfile.ZIP_DEFLATED)
+    myZip.close()
